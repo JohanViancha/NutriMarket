4800db3d6f1915ae5d985fc6492c9d2b75e7336b
Johan Viancha <vianchajohan@gmail.com>
2024-05-30 17:57:12 -0500

    :art: Adjust component and login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { ShInputComponent } from '../../shared/components/sh-input/sh-input.component';
-import { ShButtonComponent } from '../../shared/components/sh-button/sh-button.component';
-import { ShNotifyComponent } from '../../shared/components/sh-notify/sh-notify.component';
-import { NotifyService } from '../../shared/services/notify.service';
 import {
-  INCORRECT_USER_NOTIFICATION,
-  INCORRECT_USER_NOTIFICATIONs,
-} from '../../shared/models/notify';
-import { AuthService } from '../../core/services/auth.service';
-import {
+  FormBuilder,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 import { tap } from 'rxjs';
 import { User } from '../../../../db';
-import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+import { ShButtonComponent } from '../../shared/components/sh-button/sh-button.component';
+import { ShInputComponent } from '../../shared/components/sh-input/sh-input.component';
+import { ShNotifyComponent } from '../../shared/components/sh-notify/sh-notify.component';
+import { INCORRECT_USER_NOTIFICATION } from '../../shared/models/notify';
+import { SignInForm } from '../../shared/models/user';
+import { NotifyService } from '../../shared/services/notify.service';
+import { ShAlertComponent } from '../../shared/components/sh-alert/sh-alert.component';
 
 @Component({
   selector: 'app-login',
@@ -25,7 +25,9 @@ import { Router } from '@angular/router';
     ShInputComponent,
     ShButtonComponent,
     ShNotifyComponent,
+    ShAlertComponent,
     ReactiveFormsModule,
+    RouterModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
@@ -34,26 +36,27 @@ export class LoginComponent implements OnInit {
   notifyService = inject(NotifyService);
   router = inject(Router);
   authService = inject(AuthService);
-  formLogin!: FormGroup;
+  fb = inject(FormBuilder);
+  formLogin!: FormGroup<SignInForm>;
 
   ngOnInit(): void {
     this.initialForm();
     this.authService.users$.subscribe(console.log);
   }
 
-  initialForm() {
-    this.formLogin = new FormGroup({
-      user: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required]),
+  initialForm(): void {
+    this.formLogin = this.fb.group({
+      user: new FormControl<string | null>(null, [Validators.required]),
+      password: new FormControl<string | null>(null, [Validators.required]),
     });
   }
 
-  login(e: Event) {
+  login(e: Event): void {
     e.preventDefault();
     this.authService
       .login(
-        this.formLogin.get('user')?.value,
-        this.formLogin.get('password')?.value
+        this.formLogin.get('user')?.value!,
+        this.formLogin.get('password')?.value!
       )
       .pipe(
         tap((user: User[]) =>
