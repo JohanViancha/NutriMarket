07ef21f59e8333b4d86861dfb1c0f05c5f7f8dc3
Johan Viancha <vianchajohan@gmail.com>
2024-06-01 19:23:06 -0500

    :truck: Adjust routes for each module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './modules/auth/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
-      import('./auth/login.routes').then(m => m.AUTH_ROUTES),
+      import('./modules/lead/lead.routes').then((m) => m.LEAD_ROUTES),
+  },
+
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./modules/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
